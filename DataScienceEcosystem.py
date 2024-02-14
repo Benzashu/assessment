a46673f3012c1d58c8bcca76ede52f9c51e96202
Benzashu <159968834+Benzashu@users.noreply.github.com>
2024-02-13 18:17:40 -0600

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from IPython.display import display, Markdown
+
+
+# In[2]:
+
+
+display(Markdown('# Data Science Tools and Ecosystem'))
+
+
+# ### In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# #### Some of the popular languages that Data Scientists use are:
+# 1. **Python**: Widely regarded as the leading language for data science due to its simplicity and the vast array of data science libraries and frameworks it supports. Key libraries include NumPy for numerical computing, pandas for data manipulation, scikit-learn for machine learning, and TensorFlow and PyTorch for deep learning.
+# 
+# 2. **R**: Especially popular among statisticians and data miners for developing statistical software and data analysis. R is renowned for its data visualization capabilities through libraries like ggplot2 and for its extensive range of packages for statistical analysis.
+# 
+# 3. **SQL**: Essential for data science to extract and manipulate data directly from databases. Understanding SQL is crucial for data scientists to efficiently work with large datasets stored in relational databases, allowing for effective data querying, retrieval, and manipulation.
+
+# #### Some of the commonly used libraries used by Data Scientists include:
+# 1. **Pandas**: A library for data manipulation and analysis. It offers data structures and operations for manipulating numerical tables and time series, making it indispensable for data cleaning, filtering, and aggregation.
+# 
+# 2. **NumPy**: Essential for scientific computing with Python. It provides support for large, multi-dimensional arrays and matrices, along with a collection of mathematical functions to operate on these arrays, which is crucial for numerical computations.
+# 
+# 3. **Scikit-learn**: A machine learning library for Python. It features various classification, regression, and clustering algorithms, including support vector machines, random forests, gradient boosting, k-means, and DBSCAN, and is designed to interoperate with NumPy and Pandas.
+# 
+
+# | Data Science Tools |
+# |--------------------|
+# | Jupyter Notebooks  |
+# | RStudio            |
+# | VS Code with Python extension |
+# 
+
+# # Introducing arithmetic expression examples
+# 
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# 
+# - **Addition**: `5 + 3` evaluates to `8`.
+# - **Subtraction**: `10 - 2` evaluates to `8`.
+# - **Multiplication**: `4 * 2` evaluates to `8`.
+# - **Division**: `16 / 2` evaluates to `8`.
+# - **Exponentiation**: `2 ** 3` evaluates to `8`.
+# - **Modulus**: `10 % 2` evaluates to `0` (remainder of the division).
+# - **Floor Division**: `17 // 2` evaluates to `8` (the division result without the remainder).
+
+# In[3]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[5]:
+
+
+# This will convert 200 minutes to hours by dividing by 60
+hours = 200 / 60
+print(hours)
+
+
+# ## Author
+# Benjamin Ashu
+# 
+
+# In[ ]:
+
+
+
+
